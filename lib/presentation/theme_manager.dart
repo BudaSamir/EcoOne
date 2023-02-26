e2cb57e6f571f147ac624418cd2d3e40969d1ae9
Buda Samir <Abdulrahman.samir.attia.elseed.96@gmail.com>
2023-02-26 23:41:39 +0200

    Created Text Theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -6,44 +6,53 @@ import 'package:flutter/material.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-      /* Main Colors */
-      primaryColor: ColorManager.primary,
-      primaryColorLight: ColorManager.lightPrimary,
-      primaryColorDark: ColorManager.darkPrimary,
+    /* Main Colors */
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.lightPrimary,
+    primaryColorDark: ColorManager.darkPrimary,
+    disabledColor: ColorManager.grey1,
+    /* CardView Theme */
+    cardTheme: CardTheme(
+      color: ColorManager.white,
+      shadowColor: ColorManager.gery,
+      elevation: AppSize.s4,
+    ),
+    /* App Bar Theme */
+    appBarTheme: AppBarTheme(
+      centerTitle: true,
+      color: ColorManager.primary,
+      elevation: AppSize.s16,
+      shadowColor: ColorManager.lightPrimary,
+      titleTextStyle:
+          getRegularStyle(fontSize: FontSize.s16, color: ColorManager.white),
+    ),
+    /* Button Theme */
+    buttonTheme: ButtonThemeData(
+      shape: const StadiumBorder(),
       disabledColor: ColorManager.grey1,
-      /* CardView Theme */
-      cardTheme: CardTheme(
-        color: ColorManager.white,
-        shadowColor: ColorManager.gery,
-        elevation: AppSize.s4,
-      ),
-      /* App Bar Theme */
-      appBarTheme: AppBarTheme(
-        centerTitle: true,
-        color: ColorManager.primary,
-        elevation: AppSize.s16,
-        shadowColor: ColorManager.lightPrimary,
-        titleTextStyle:
-            getRegularStyle(fontSize: FontSize.s16, color: ColorManager.white),
-      ),
-      /* Button Theme */
-      buttonTheme: ButtonThemeData(
-        shape: const StadiumBorder(),
-        disabledColor: ColorManager.grey1,
-        buttonColor: ColorManager.primary,
-        splashColor: ColorManager.lightPrimary,
-      ),
-      /* ElevatedButton Button Theme */
-      elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-              textStyle: getRegularStyle(
-                  color: ColorManager.white, fontSize: FontSize.s17),
-              backgroundColor: ColorManager.primary,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(AppSize.s12))))
-      /* Text Theme */
+      buttonColor: ColorManager.primary,
+      splashColor: ColorManager.lightPrimary,
+    ),
+    /* ElevatedButton Button Theme */
+    elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+            textStyle: getRegularStyle(
+                color: ColorManager.white, fontSize: FontSize.s17),
+            backgroundColor: ColorManager.primary,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(AppSize.s12)))),
+    /* Text Theme */
+    textTheme: TextTheme(
+      displayLarge:
+          getLightStyle(color: ColorManager.white, fontSize: FontSize.s22),
+      headline1: getSemiBoldStyle(
+          color: ColorManager.darkGrey, fontSize: FontSize.s16),
+      subtitle1:
+          getMediumStyle(color: ColorManager.lightGrey, fontSize: FontSize.s14),
+      caption: getRegularStyle(color: ColorManager.grey1),
+      bodyText1: getRegularStyle(color: ColorManager.gery),
+    ),
 
-      /* Input Decoration Theme */
-
-      );
+    /* Input Decoration Theme */
+  );
 }
