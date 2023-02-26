c278cb3ada0f6d00140e88c48347e403479ac586
Buda Samir <Abdulrahman.samir.attia.elseed.96@gmail.com>
2023-02-26 17:30:39 +0200

    created color manager

---
+++ lib/presentation/color_manager.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static Color primary = const Color(0xffED9728); // "#ED9728"
+  static Color darkGrey = const Color(0xff525252); // "#525252"
+  static Color gery = const Color(0xff737477); //  "#737477"
+  static Color lightGrey = const Color(0xff9E9E9E); // "#9E9E9E"
+
+}
