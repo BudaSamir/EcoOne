2f62ae59a04047cd003a29ce4a7f2940abf43a35
Buda Samir <Abdulrahman.samir.attia.elseed.96@gmail.com>
2023-02-26 22:47:37 +0200

    Created CardView Them

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,18 +1,23 @@
 import 'package:eco_one/presentation/color_manager.dart';
+import 'package:eco_one/presentation/values_manager.dart';
 import 'package:flutter/material.dart';
 
 ThemeData getApplicationTheme() {
   return ThemeData(
-      /* Main Colors */
-      primaryColor: ColorManager.primary,
-      primaryColorLight: ColorManager.lightPrimary,
-      primaryColorDark: ColorManager.darkPrimary,
-      disabledColor: ColorManager.grey1
-      /* Card View Theme */
-      /* App Bar Theme */
-      /* Buttom Theme */
-      /* Text Theme */
-      /* Input Decoration Theme */
-
-      );
+    /* Main Colors */
+    primaryColor: ColorManager.primary,
+    primaryColorLight: ColorManager.lightPrimary,
+    primaryColorDark: ColorManager.darkPrimary,
+    disabledColor: ColorManager.grey1,
+    /* CardView Theme */
+    cardTheme: CardTheme(
+      color: ColorManager.white,
+      shadowColor: ColorManager.gery,
+      elevation: AppSize.s4,
+    ),
+    /* App Bar Theme */
+    /* Buttom Theme */
+    /* Text Theme */
+    /* Input Decoration Theme */
+  );
 }
