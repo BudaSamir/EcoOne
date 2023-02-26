b2ecc89f28675a709a1317bc7d11e69c27c20ef4
Buda Samir <Abdulrahman.samir.attia.elseed.96@gmail.com>
2023-02-26 20:58:02 +0200

    Creating Style Manager

---
+++ lib/presentation/styles_manager.dart
@@ -0,0 +1,63 @@
+import 'package:eco_one/presentation/fonts_manager.dart';
+import 'package:flutter/material.dart';
+
+// Created Method Return TextStyle Widget (TextStyle base)
+TextStyle _getTextStyleWidget(
+    double fontSize, FontWeight fontWeight, Color color) {
+  return TextStyle(fontSize: fontSize, fontWeight: fontWeight, color: color);
+}
+
+/* 
+  Created Method Return Light TextStyle 
+  ( Used The TrstStyle Base (_getTextStyleWidget) Method )
+*/
+TextStyle getLightStyle(
+    {double fontSize = FontSizeManager.s12,
+    FontWeight fontWeight = FontWeightManager.light,
+    required Color color}) {
+  return _getTextStyleWidget(fontSize, fontWeight, color);
+}
+
+/* 
+  Created Method Return Regular TextStyle 
+  ( Used The TrstStyle Base (_getTextStyleWidget) Method )
+*/
+TextStyle getRegularStyle(
+    {double fontSize = FontSizeManager.s12,
+    FontWeight fontWeight = FontWeightManager.regular,
+    required Color color}) {
+  return _getTextStyleWidget(fontSize, fontWeight, color);
+}
+
+/* 
+  Created Method Return Medium TextStyle 
+  ( Used The TrstStyle Base (_getTextStyleWidget) Method )
+*/
+TextStyle getMediumStyle(
+    {double fontSize = FontSizeManager.s12,
+    FontWeight fontWeight = FontWeightManager.medium,
+    required Color color}) {
+  return _getTextStyleWidget(fontSize, fontWeight, color);
+}
+
+/* 
+  Created Method Return Semi Bold TextStyle 
+  ( Used The TrstStyle Base (_getTextStyleWidget) Method )
+*/
+TextStyle getSemiBoldStyle(
+    {double fontSize = FontSizeManager.s12,
+    FontWeight fontWeight = FontWeightManager.semiBold,
+    required Color color}) {
+  return _getTextStyleWidget(fontSize, fontWeight, color);
+}
+
+/* 
+  Created Method Return Bold TextStyle 
+  ( Used The TrstStyle Base (_getTextStyleWidget) Method )
+*/
+TextStyle getBoldStyle(
+    {double fontSize = FontSizeManager.s12,
+    FontWeight fontWeight = FontWeightManager.bold,
+    required Color color}) {
+  return _getTextStyleWidget(fontSize, fontWeight, color);
+}
